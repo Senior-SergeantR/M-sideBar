c48dd8621cef93a0887b836f75c2961d3a207225
malyumohamed <malyunmohaz942@gmail.com>
2023-10-25 19:18:38 +0300

    updated some CSS HTML and Javascript

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let btn= document.querySelector(".fa-bars");
+let btn = document.querySelector(".fa-bars");
 let sidebar = document.querySelector(".sidebar");
 
-btn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
+
+btn.addEventListener("click", ()=> {
+  sidebar.classList.toggle("close");
 });
 
-let arrows=document.querySelector("arrow");
-for(var i=0; i < arrows.length ;i++ ){
-    arrows[1].addEventListener("click", (e)=>{
-        let arrowparent= e .target.parentElement.parentElement;
 
-        arrowparent.classList.toggle("show");
-
-
-    })
+let arrows = document.querySelectorAll(".arrow");
+for(var i = 0; i < arrows.length; i++){
+  arrows[i].addEventListener("click",(e) =>{
+  let arrowparent = e.target.parentElement.parentElement;
+  arrowparent.classList.toggle("show");
+  })
 }
